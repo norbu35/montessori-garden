e7978774d6afa1d720b43acf2d7c15b42ba8b48e
norbu <93921032+norbu35@users.noreply.github.com>
2023-03-21 03:13:55 +0800

    sitemap / privacy policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true,
   },
+  site: "https://montessorigarden.netlify.app",
+  integrations: [sitemap()],
 });
