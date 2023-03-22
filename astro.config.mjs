2252888abf3b75b8fb10742e2a46ee987692d999
norbu <93921032+norbu35@users.noreply.github.com>
2023-03-22 13:52:13 +0800

    news section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   site: "https://montessorigarden.netlify.app",
-  integrations: [sitemap()],
+  integrations: [sitemap(), mdx()]
 });
