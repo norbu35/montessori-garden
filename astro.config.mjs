fa3f7aaf9973d515c2ebbe269ea0a28b53564d99
norbu <93921032+norbu35@users.noreply.github.com>
2023-04-01 17:44:28 +0800

    added sharp image converter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   site: "https://montessorigarden.netlify.app",
   output: "server",
   adapter: netlify(),
   integrations: [sitemap(), mdx()],
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: "astro/assets/services/sharp",
+  },
 });
